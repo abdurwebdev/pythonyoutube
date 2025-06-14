bfa8b8c37491fa6ac6ec56701b239fc433dce617
abdurwebdev <iabdurrehman22.0@gmail.com>
2025-06-14 14:55:27 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+print("Hello");
+#this is a comment
+"""hello this is a multi line
+comment
+"""
+
+sher = "Student"
+
+SgeryiansSchool = "Student"
+
+sheryiansScholl = "Student"
+
+sheryians_scholl = "Student"
+
+print(sher);
+
+sher = -10
+sher2 = 10.9
+sher3 = 12/4
+
+sher4 = True
+sher5 = False
+
+sher6 = 'hello'
+sher6 = "hello"
+
+sher7 = 14j
+
+a="A"
+
+print(ord(a))
+
+b=65
+
+print(chr(b));
+
+c="Hello"
+
+print(c[2],c[-3]);
+
+d="SHER CODER"
+print(d[0:4:1]);
+
+e="SHER CODER"
+print(e[5::1])
+
+f="SHER CODER"
+print(e[::])
+
+g=12
+g=str(g)
+print(type(g))
+
+h="12"
+h=int(h)
+print(type(h))
+
+# 0 false "" 0.0 [] {} () Falsy
+
+age = 12
+name ="Abdurrehman"
+
+print(f"hello my name is {name} and my age is {age}");
+
+name2=input("Enter your name ");
+age2=int(input("Enter your age "));
+
+print(f"my name is {name2} and my age is {age2}");
+
+i=10
+j=20
+
+print(i+j)
+print(i-j)
+print(i*j)
+print(5**2)
+print(20/5)
+print(20//5)
+print(32%5)
