fb607c1987baa59703e2ad05b52e4410d1a2e15d
abdurwebdev <iabdurrehman22.0@gmail.com>
2025-06-14 17:08:55 +0500

    Day 2

--- main.py
+++ main.py
@@ -77,3 +77,69 @@ print(5**2)
 print(20/5)
 print(20//5)
 print(32%5)
+
+#assignment operators
+
+k=20
+k+=20
+k+=40
+k+=60
+print(k)
+"""
++=
+-=
+*=
+/=
+//=
+%=
+**=
+"""
+print(12==12)
+print(23>10)
+print(10<30)
+print(23<=23)
+print(45>=45)
+print(12!=11)
+
+print("A"=="A")
+print("ABC"<"ACD");
+#not done print("A">34)
+
+print(12<1 and 12>2 and 30<60)
+print(12<3 or 12>20 or 89<40)
+print(not 12==12)
+
+
+m=6
+if m>10:
+  print("I will do Task A")
+else:
+  print("I will do Task B")
+money = int(input("Please Provide me the money "))
+
+if money==10:
+  print("I will have a chocobar icecream")
+else:
+  print("I will have a mango dolly")
+
+
+choice = int(input("Please Provide me the Money -:>"))
+
+if choice==10:
+  print("I will have a chocobar icecream")
+elif choice==20:
+  print("I will have a mango dolly")
+elif choice==30:
+  print("I will have a chocobar icecream and a mango dolly")
+else:
+  print("You provided me more money")
+
+num1=int(input("Enter Fistr number: "))
+num2=int(input("Enter Second Number: "))
+
+if num1>num2:
+  print(f"{num1} is greater than {num2}")
+elif num2>num1:
+  print(f"{num2} is greater than {num1}")
+else:
+  print(f"{num1} is equal to {num2}")
